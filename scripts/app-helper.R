38e33f880427e4d1e6a96282b1a4c3bd57c61983
nurakawa <nkawa@berkeley.edu>
2017-12-20 17:58:46 -0800

    removed extra dataframes

--- scripts/app-helper.R
+++ scripts/app-helper.R
@@ -40,16 +40,6 @@ ca <- ca[ca$starbucks_count>0,]
 ca <- ca[rowSums(is.na(ca))==0,]
 
 
-##### 
-# add in starbucks and peets counts
-
-#starbucks_ca <- ca %>% filter(starbucks_count>0) 
-#peets_ca <- ca %>% filter(peets_count>0)
-
-#shop_data <- list("starbucks" = starbucks_ca,
-#                  "peets" = peets_ca)
-
-
 #####
 # color palettes
 
@@ -58,22 +48,8 @@ starbuckspal <- colorFactor(c("darkolivegreen1",
                               "black"),
                     domain = c("low", "med","high"))
 
-peetspal <- colorNumeric(c("tan3",
-                           "tan4",
-                           "chocolate3",
-                           "chocolate4",
-                           "black"),
-                         domain = 1:max(ca$peets_count))
-
 #####
-# for the app
-# shop_pallete <- list("starbucks" = starbuckspal,
-#                      "peets" = peetspal)
-# 
-# shop_label <- list("starbucks" = shop_data$starbucks$starbucks_count,
-#                    "peets" = shop_data$peets$peets_count)
-#####
-# radius bins 
+# bins 
 
 make_bins <- function(shop_count){
   binned_count=cut(shop_count, 
@@ -82,57 +58,8 @@ make_bins <- function(shop_count){
   return(binned_count)
 }
 
-  
-make_bins(ca$starbucks_count)
-#ca$peets_count <= 1
-
 ##### 
-# data for pie chart
-shops <- data.frame(
-  "city" = ca$city,
-  "shops" = ca$starbucks_count+ca$peets_count,
-  "starbucks" = ca$starbucks_count,
-  "peets" = ca$peets_count
-)
-
-# bubble <- data.frame(
-#   "city" = ca$city,
-#   "median_income_per_capita" = ca$median_household_income/ca$pop,
-#   "median_age" = ca$median_age,
-#   peets = ca$peets_count,
-#   #"peets" = as.numeric(set_radius(ca$peets_count)),
-#   "starbucks" = as.numeric(set_radius(ca$starbucks_count))
-# )
-# 
-# 
-
-bar <- data.frame(
-  "city" = ca$city,
-  "stbks_pc" = round(ca$starbucks_count/ca$pop,5),
-  "rank" = rank(ca$starbucks_count/ca$pop)
-  #"starbucks_p_ft" = ca$starbucks_count/ca$land_area
-  #"med_age" = ca$median_age,
-  #"med_income" = ca$median_household_income/ca$pop
-)
-
-bar# <- bar %>% arrange(stbks_pc) %>% tail(5)
-
-plot(scale(ca$median_age),
-     scale(ca$starbucks_count/ca$pop))
-
-plot(make_bins(ca$median_household_income),
-     make_bins(ca$starbucks_count/ca$pop))
-
-plot(make_bins(ca$median_age),
-     make_bins(ca$starbucks_count/ca$pop),
-     xlab = "Median Age",
-     ylab = "#Starbucks per capita")
-
-plot(make_bins(ca$land_area),
-     make_bins(ca$starbucks_count/ca$pop),
-     xlab = "Median Household Income",
-     ylab = "#Starbucks per capita")
-
+# data for valuebox
 
 df_valueBox <- data.frame(
   "city" = ca$city,
@@ -144,6 +71,4 @@ df_valueBox <- data.frame(
   "bin_pop" = make_bins(ca$pop),
   "bin_star" = make_bins(ca$starbucks_count)
 )
-
-
 
